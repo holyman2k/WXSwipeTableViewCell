418dbb4b84ab57ed9e6b08db3deb2217769e9f67
Charlie Wu <c.wu@webfm.net>
2014-01-23 14:49:33 +1100

    Initial Commit

---
+++ WXSwipeTableViewCell/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WXSwipeTableViewCell
+//
+//  Created by Charlie Wu on 23/01/2014.
+//  Copyright (c) 2014 Charlie Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
